7224a976178efb5d8992c03fb2ae133b1cba95a4
Liezel Paciente <124854305+mecazeli@users.noreply.github.com>
2024-11-22 14:14:39 +0800

    Add files via upload

---
+++ ITP104-PROJECT-COMPANY.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE company;
+
+CREATE TABLE department(
+     departmentId INT PRIMARY KEY AUTO_INCREMENT,
+     departmentName VARCHAR(100),
+     description VARCHAR(150)
+);
+
+INSERT INTO department(departmentName,description) VALUES 
+     ("IT Department", "Oversees technology, cybersecurity, and digital operations, ensuring seamless innovation in Velvaire's beauty solutions.");
+	
+SELECT * FROM department;
