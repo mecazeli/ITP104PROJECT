abf78b6e0864091dd0dc346efcc04996fcc06482
[Lawrence Mindanao] <[mindanaolawrence86@gmail.com]>
2024-11-23 09:36:29 +0800

    updated the methods [tested na siya] -kulang nlng yung mga error handling sa pag delete and update kung wala ka selected cells .WIP

--- ITP104-PROJECT-COMPANY.sql
+++ ITP104-PROJECT-COMPANY.sql
@@ -1,12 +1,13 @@
-CREATE DATABASE company;
-USE company;
-CREATE TABLE department(
-     departmentId INT PRIMARY KEY AUTO_INCREMENT,
-     departmentName VARCHAR(100),
-     description VARCHAR(150)
-);
+-- CREATE DATABASE company;
+-- USE company;
+-- CREATE TABLE department(
+--      departmentId INT PRIMARY KEY AUTO_INCREMENT,
+--      departmentName VARCHAR(100),
+--      description VARCHAR(150)
+-- );
 
-INSERT INTO department(departmentName,description) VALUES 
-     ("IT Department", "Oversees technology, cybersecurity, and digital operations, ensuring seamless innovation in Velvaire's beauty solutions.");
+-- INSERT INTO department(departmentName,description) VALUES 
+--      ("IT Department", "Oversees technology, cybersecurity, and digital operations, ensuring seamless innovation in Velvaire's beauty solutions.");
 	
 SELECT * FROM department;
+
