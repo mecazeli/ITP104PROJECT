11ca28043a19c1c83048c03a7911aff89d5993d0
[Lawrence Mindanao] <[mindanaolawrence86@gmail.com]>
2024-12-06 22:32:03 +0800

    started func on project(wip)

--- ITP104PROJECT/Project.cs
+++ ITP104PROJECT/Project.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySqlConnector;
 
 namespace ITP104PROJECT
 {
     public partial class Project : Form
     {
+
+        public static string connection = "server=localhost; user=root; password=; database=company; port=3306";
+        //public static string connection = "server=localhost; user=root; password=liezel11; database=company;";
+        public MySqlConnection conn;
+        public Admin admin = new Admin("Liezel T. Paciente", 30, "Female", "admin101", "password123");
+
         public Project()
         {
             InitializeComponent();
@@ -25,7 +33,7 @@ namespace ITP104PROJECT
       
         private void Project_Load(object sender, EventArgs e)
         {
-
+            label4.Text = admin.name;
         }
 
         private void btnSide_Click(object sender, EventArgs e)
@@ -89,10 +97,63 @@ namespace ITP104PROJECT
         }
 
 
+        private void AddProject()
+        {
+            
+        }
+        private void DeleteProejct()
+        {
+
+        }
+        private void UpdateProject()
+        {
+
+        }
+
+        private void btnAddProject_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDeleteProject_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdateProject_Click(object sender, EventArgs e)
+        {
+
+        }
 
 
+        // +==============================================================+
 
+        private void AddTask()
+        {
 
+        }
+        private void DeleteTask()
+        {
 
+        }
+        private void UpdateTask()
+        {
+
+        }
+
+        private void btnAddTask_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDeleteTask_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdateTask_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
